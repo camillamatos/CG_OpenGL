bce615de9c1bbc369d457add90444b4a0007c7b7
Camilla Pozer de Matos <camillapozerm@gmail.com>
2020-04-08 15:42:15 -0300

    Update colisao.cpp

--- colisao.cpp
+++ colisao.cpp
@@ -31,9 +31,7 @@ int main(int argc, char** argv) {
 	return(0);
 }
 
-
-void MouseClick(int button, int state, int x, int y)
-{
+void MouseClick(int button, int state, int x, int y){
 	int pos = 0;
 	if (button == GLUT_LEFT_BUTTON) {
 		if (x > 400) {
@@ -44,26 +42,22 @@ void MouseClick(int button, int state, int x, int y)
 		}
 	}
 	
-	
 	pos = l[1] - l[0];
 
 	if (pos < 0) {
 		pos *= -1;
 	}
 
-
 	if (pos <= 100) {
 		l[0] = -400;
 		l[1] = 400;
 	}
 	glutPostRedisplay();
-
 }
 
 void bola() {
 	GLfloat circ_pnt = 100;
 	GLfloat ang, raioX = 50.0f, raioY = 50.0f;
-
 
 	glBegin(GL_POLYGON);
 	for (int i = 0; i < circ_pnt; i++) {
@@ -80,9 +74,7 @@ void linha() {
 	glEnd();
 }
 
-
 void desenhar() {
-
 	glPushMatrix();
 	glTranslatef(l[0], 0, 0);
 
@@ -101,8 +93,6 @@ void desenhar() {
 	glLineWidth(3.0f);
 	glColor3f(1, 0, 0);
 	linha();
-
-
 }
 
 void display() {
@@ -128,8 +118,3 @@ void tela(GLsizei w, GLsizei h) {
 
 	glMatrixMode(GL_MODELVIEW);
 }
-
-
-
-
-
